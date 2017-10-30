e98c8511fddf26d7b6918a57b7c9d0189f8ebd06
Karl Ehatäht <karlehataht@gmail.com>
2017-10-30 12:57:04 +0200

    Add module for finding Higgs decay mode at generator level
    Based on Heppy code: https://github.com/vhbb/cmssw/blob/81148c34bb3b546425be61af523c4a7847253484/VHbbAnalysis/Heppy/python/VHGeneratorAnalyzer.py#L324-L325

---
+++ python/postprocessing/modules/genHiggsDecayModeProducer.py
@@ -0,0 +1,45 @@
+import ROOT
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+
+from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection
+from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
+
+class genHiggsDecayModeProducer(Module):
+
+  def __init__(self):
+    self.genHiggsDecayModeName = "genHiggsDecayMode"
+
+  def beginJob(self):
+    pass
+
+  def endJob(self):
+    pass
+
+  def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
+    self.out = wrappedOutputTree
+    self.out.branch(self.genHiggsDecayModeName, "F")
+
+  def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
+    pass
+
+  def analyze(self, event):
+    genParticles = Collection(event, "GenPart")
+
+    genHiggsDecayModeVal = 0
+    nofHiggs = len(list(filter(lambda genParticle: genParticle.pdgId == 25, genParticles)))
+    if nofHiggs > 0:
+      h0_daus = list(filter(
+        lambda genParticle: genParticle.genPartIdxMother >= 0 and \
+                            genParticles[genParticle.genPartIdxMother].pdgId == 25 and \
+                            genParticle.pdgId != 25,
+        genParticles
+      ))
+      genHiggsDecayModeVal  = abs(h0_daus[0].pdgId if len(h0_daus) >= 1 else 0)
+      genHiggsDecayModeVal += abs(h0_daus[1].pdgId if len(h0_daus) >= 2 and \
+                                                      abs(h0_daus[0].pdgId) != abs(h0_daus[1].pdgId) else 0) * 10000
+
+    self.out.fillBranch(self.genHiggsDecayModeName, genHiggsDecayModeVal)
+    return True
+
+# provide this variable as the 2nd argument to the import option for the nano_postproc.py script
+genHiggsDecayMode = lambda : genHiggsDecayModeProducer()
