d797769f9e6f8586e27179b95ea9d50d4bc4d2f8
Karl Ehatäht <karlehataht@gmail.com>
2018-02-08 15:39:16 +0200

    Do not edit cmsDriver cfgs in-place but use a static one
    Also provided command line options for the launchall script:
      -f the input dataset file (mandatory)
      -d for performing CRAB dryrun
      -D cfg file for data jobs
      -M cfg file for MC jobs
    
    The only downside currently is that some of the settings (e.g. if a given sample is data or MC, global tags and JSON lumi file) are hardcoded at least twice: in launchall.sh and the alternative static config files (in our case it's the cfg file to run jetSubstructure jobs). I don't think it's possible to propagate such settings to these static config files via environment variables as we do for the CRAB cfgs.

---
+++ test/nano_cfg_jetSubstructure.py
@@ -0,0 +1,75 @@
+import FWCore.ParameterSet.Config as cms
+import FWCore.ParameterSet.Types as CfgTypes
+import FWCore.PythonUtilities.LumiList as LumiList
+
+from Configuration.StandardSequences.Eras import eras
+from Configuration.AlCa.autoCond import autoCond
+
+from tthAnalysis.NanoAOD.addJetSubstructureObservables import addJetSubstructureObservables
+
+import os.path
+
+isMC            = True
+global_tag_data = '94X_dataRun2_ReReco_EOY17_v2'
+global_tag_mc   = '94X_mc2017_realistic_v10'
+JSONfile = os.path.join(
+  os.path.dirname(os.path.realpath(__file__)), '..', 'data', 'Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON.txt'
+)
+
+process = cms.Process('NANO', eras.Run2_2017)
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.load("Configuration.StandardSequences.GeometryDB_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.load('Configuration.StandardSequences.Services_cff')
+process.load("Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff")
+process.load("Configuration.Geometry.GeometryRecoDB_cff")
+process.load("RecoBTag.Configuration.RecoBTag_cff")
+process.load("PhysicsTools.NanoAOD.nano_cff")
+
+process.options   = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+
+process.source = cms.Source(
+  "PoolSource",
+  fileNames = cms.untracked.vstring(),
+)
+
+process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
+  calibratedPatElectrons = cms.PSet(
+    initialSeed = cms.untracked.uint32(81),
+    engineName  = cms.untracked.string('TRandom3'),
+  ),
+  calibratedPatPhotons = cms.PSet(
+    initialSeed = cms.untracked.uint32(81),
+    engineName  = cms.untracked.string('TRandom3'),
+  ),
+)
+
+if isMC:
+  process.nanoPath = cms.Path(process.nanoSequenceMC)
+  process          = nanoAOD_customizeMC(process)
+  process.GlobalTag.globaltag = global_tag_mc
+else:
+  process.nanoPath = cms.Path(process.nanoSequence)
+  process          = nanoAOD_customizeData(process)
+  process.GlobalTag.globaltag = global_tag_data
+
+  process.source.lumisToProcess = CfgTypes.untracked(CfgTypes.VLuminosityBlockRange())
+  lumisToProcess = LumiList.LumiList(filename = JSONfile).getCMSSWString().split(',')
+  process.source.lumisToProcess.extend(lumisToProcess)
+
+#--------------------------------------------------------------------------------
+# CV: customization with jet substructure observables for hadronic top reconstruction (boosted and non-boosted)
+addJetSubstructureObservables(process, isMC)
+#--------------------------------------------------------------------------------
+
+process.out = cms.OutputModule("NanoAODOutputModule",
+    fileName       = cms.untracked.string('tree.root'),
+    outputCommands = process.NanoAODEDMEventContent.outputCommands,
+    #compressionLevel = cms.untracked.int32(9),
+    #compressionAlgorithm = cms.untracked.string("LZMA"),
+)
+
+process.end = cms.EndPath(process.out)
