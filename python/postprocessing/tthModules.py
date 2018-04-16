1251bd914b889b801f8c03d22ae3f72cec8e104e
Karl Ehatäht <karlehataht@gmail.com>
2018-04-16 20:49:23 +0300

    Import all lambdas from lepJetVarProducer module
    Forgot to do so in https://github.com/HEP-KBFI/tth-nanoAOD-tools/commit/c4995f1aae86c2625872a28cba7cff1ecf0c48fa

--- python/postprocessing/tthModules.py
+++ python/postprocessing/tthModules.py
@@ -1,5 +1,5 @@
 from tthAnalysis.NanoAODTools.postprocessing.modules.genParticleProducer import genAll
-from tthAnalysis.NanoAODTools.postprocessing.modules.lepJetVarProducer import lepJetVarBTagAll
+from tthAnalysis.NanoAODTools.postprocessing.modules.lepJetVarProducer import *
 from tthAnalysis.NanoAODTools.postprocessing.modules.genHiggsDecayModeProducer import genHiggsDecayMode
 from tthAnalysis.NanoAODTools.postprocessing.modules.tauIDLogProducer import tauIDLog_2016, tauIDLog_2017
 from tthAnalysis.NanoAODTools.postprocessing.modules.absIsoProducer import absIso
