HEP-KBFI/tth-nanoAOD-tools

commit 48bc9d192ef49daf1591ce4b68e1ce3679d0b57b

Compute gen Higgs decay mode only if there's 1 Higgs in the event
Currently, we're only interest in hh->4tau sample and right now I don't foresee that we want to process di-Higgs samples w/ some other decay modes. In other words, the Higgs decay mode becomes irrelevant if the nof Higgs > 1. Saves computing time.

python/postprocessing/modules/genHiggsDecayModeProducer.py
@@ -27,7 +27,7 @@ class genHiggsDecayModeProducer(Module):
 
     genHiggsDecayModeVal = 0
     nofHiggs = len(list(filter(lambda genParticle: genParticle.pdgId == 25, genParticles)))
-    if nofHiggs > 0:
+    if nofHiggs == 1:
       h0_daus = list(filter(
         lambda genParticle: genParticle.genPartIdxMother >= 0 and \
                             genParticles[genParticle.genPartIdxMother].pdgId == 25 and \

commit 44711dc06b611fcb51879dc8d00702de80356341

Correctly count the nof Higgs bosons in genHiggsDecayModeProducer
Bug introduced in 48bc9d192ef49daf1591ce4b68e1ce3679d0b57b. Forgot about the fact that the GenPart collection contains multiple Higgs bosons w/ different statuses. The Higgs multiplicity is determined by counting the unique number of ,,earlier'' Higgs bosons (i.e. Higgs bosons whose parents are not Higgs).

python/postprocessing/modules/genHiggsDecayModeProducer.py
@@ -26,7 +26,13 @@ class genHiggsDecayModeProducer(Module):
     genParticles = Collection(event, "GenPart")
 
     genHiggsDecayModeVal = 0
-    nofHiggs = len(list(filter(lambda genParticle: genParticle.pdgId == 25, genParticles)))
+    nofHiggs = len(list(filter(
+      lambda genPart:
+        genPart.pdgId == 25 and \
+        (genParticles[genPart.genPartIdxMother].pdgId != 25 if genPart.genPartIdxMother >= 0 else True),
+      genParticles
+    )))
+
     if nofHiggs == 1:
       h0_daus = list(filter(
         lambda genParticle: genParticle.genPartIdxMother >= 0 and \