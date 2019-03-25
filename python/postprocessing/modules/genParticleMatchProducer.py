1b2c034b5ecf95e726fd5e53894aee65fc300423
ktht <karlehataht@gmail.com>
2019-03-25 16:28:01 +0200

    Match reco ele, mu, tau to gen jets

--- python/postprocessing/modules/genParticleMatchProducer.py
+++ python/postprocessing/modules/genParticleMatchProducer.py
@@ -8,6 +8,28 @@ from tthAnalysis.NanoAODTools.postprocessing.modules.genParticleProducer import 
 
 import collections
 
+class GenJetAux:
+  def __init__(self, genJet, idx):
+    self.pt               = genJet.pt
+    self.eta              = genJet.eta
+    self.phi              = genJet.phi
+    self.mass             = genJet.mass
+    self.pdgId            = genJet.partonFlavour
+    self.charge           = 0
+    self.status           = -1
+    self.statusFlags      = -1
+    self.genPartIdxMother = -1
+    self.idx              = idx
+
+  def __str__(self):
+    return "pt = %.3f eta = %.3f phi = %.3f mass = %.3f pdgId = %i charge = %i status = %i " \
+           "statusFlags = %i mom = %i idx = %i" % \
+      (self.pt, self.eta, self.phi, self.mass, self.pdgId, self.charge, self.status, \
+       self.statusFlags, self.genPartIdxMother, self.idx)
+
+  def __repr__(self):
+    return self.__str__()
+
 def getDefaultValue(genVar):
   if genVar in [ 'status', 'statusFlags' ]:
     return -1
@@ -19,7 +41,10 @@ def getDefaultValue(genVar):
 class genParticleMatchProducer(Module):
   def __init__(self):
     self.massTable = MassTable()
-    self.genPartBr = 'GenPart'
+
+    self.genPartBr    = 'GenPart'
+    self.genPartJetBr = 'GenJet'
+    self.genPartTauBr = 'GenVisTau'
 
     self.muBr = 'Muon'
     self.elBr = 'Electron'
@@ -52,7 +77,7 @@ class genParticleMatchProducer(Module):
   def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
     self.out = wrappedOutputTree
     for recoObjBr in [ self.muBr, self.elBr, self.taBr ]: #, self.jtBr ]:
-      for genObjBr in [ self.genLepBr, self.genPhoBr, self.genOthBr ]:#, self.genTauBr, self.genJetBr ]:
+      for genObjBr in [ self.genLepBr, self.genPhoBr, self.genOthBr, self.genJetBr ]:#, self.genTauBr, self.genJetBr ]:
         for genVar in self.genVars:
           self.out.branch('_'.join([ recoObjBr, genObjBr, genVar ]), self.genVars[genVar], lenVar = 'n{}'.format(recoObjBr))
 
@@ -64,6 +89,12 @@ class genParticleMatchProducer(Module):
       idx : GenPartAux(part, idx, self.massTable)
       for idx, part in enumerate(Collection(event, self.genPartBr))
     }
+    genJets = {
+      idx : GenJetAux(part, idx)
+      for idx, part in enumerate(Collection(event, self.genPartJetBr))
+    }
+    jets = Collection(event, self.jtBr)
+
     genOut = {}
     for recoObjBr in [ self.muBr, self.elBr, self.taBr ]:#, self.jtBr ]:
       genOut[recoObjBr] = {}
@@ -74,6 +105,7 @@ class genParticleMatchProducer(Module):
       recoObjs = Collection(event, recoObjBr)
 
       for recoObj in recoObjs:
+
         if recoObj.genPartIdx >= 0:
           genPart = genParticles[recoObj.genPartIdx]
 
@@ -96,8 +128,18 @@ class genParticleMatchProducer(Module):
           for genObjBr in [ self.genLepBr, self.genPhoBr, self.genOthBr ]:
             genOut[recoObjBr][genObjBr].append(None)
 
+        if recoObj.jetIdx >= 0:
+          jet = jets[recoObj.jetIdx]
+
+          if jet.genJetIdx >= 0:
+            genOut[recoObjBr][self.genJetBr].append(genJets[jet.genJetIdx])
+          else:
+            genOut[recoObjBr][self.genJetBr].append(None)
+        else:
+          genOut[recoObjBr][self.genJetBr].append(None)
+
     for recoObjBr in [self.muBr, self.elBr, self.taBr ]:#, self.jtBr]:
-      for genObjBr in [self.genLepBr, self.genPhoBr, self.genOthBr ]:#, self.genTauBr, self.genJetBr]:
+      for genObjBr in [self.genLepBr, self.genPhoBr, self.genOthBr, self.genJetBr ]:# self.genTauBr
         genOutArr = genOut[recoObjBr][genObjBr]
         for genVar in self.genVars:
           genVarArr = map(lambda genObj: getattr(genObj, genVar) if genObj is not None else getDefaultValue(genVar), genOutArr)
