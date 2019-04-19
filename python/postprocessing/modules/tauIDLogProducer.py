a8973a75c72716546f1183f920bddc99d30282fa
ktht <karlehataht@gmail.com>
2019-04-19 11:09:58 +0300

    Disable the linearization of DNN and DPF tau ID discriminators
    We'll never produce Ntuples w/ them due to overly large memory consumption. DNN v2 is currently in the works which will eventually replace the disabled discriminators.

--- python/postprocessing/modules/tauIDLogProducer.py
+++ python/postprocessing/modules/tauIDLogProducer.py
@@ -24,8 +24,8 @@ class tauIDLogProducer(Module):
     self.tauBr_ids = [
       'idAntiMu', 'idAntiEle', 'idAntiEle2018',
       'idMVAnewDM2017v2', 'idMVAoldDM', 'idMVAoldDMdR032017v2', 'idMVAoldDM2017v1', 'idMVAoldDM2017v2',
-      'idDeepTau2017v1VSe', 'idDeepTau2017v1VSmu', 'idDeepTau2017v1VSjet',
-      'idDpfTau2016v0VSall',
+      #'idDeepTau2017v1VSe', 'idDeepTau2017v1VSmu', 'idDeepTau2017v1VSjet',
+      #'idDpfTau2016v0VSall',
       # no WPs in DPFTau2016v1
     ]
     self.tauBr_ids  = {
