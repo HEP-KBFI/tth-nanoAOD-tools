2c026cd32eecc3dc34408b820e99df19359db1a7
ktht <karlehataht@gmail.com>
2019-06-05 18:34:34 +0300

    Normalize the tH cross section to the value that corresponds to kv = 1.0

--- python/tHweights_cfi.py
+++ python/tHweights_cfi.py
@@ -1,10 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
-def get_tHq_sf(kt, kv):
-  return 1.0 if kt == 1.0 and kv == 1.0 else 2.63 * kt * kt + 3.588 * kv * kv - 5.21 * kt * kv
+KV_NORM = 1.0
 
-def get_tHW_sf(kt, kv):
-  return 1.0 if kt == 1.0 and kv == 1.0 else 2.91 * kt * kt + 2.31 * kv * kv - 4.22 * kt * kv
+def get_tHq_sf(kt_over_kv, kv):
+  return 1.0 if kt_over_kv == 1.0 and kv == 1.0 else kv**2 * (2.63 * kt_over_kv**2 + 3.588 - 5.21 * kt_over_kv)
+
+def get_tHW_sf(kt_over_kv, kv):
+  return 1.0 if kt_over_kv == 1.0 and kv == 1.0 else kv**2 * (2.91 * kt_over_kv**2 + 2.31 - 4.22 * kt_over_kv)
 
 tHweights = cms.VPSet(
   cms.PSet(kt = cms.double(-1.0),  kv = cms.double(1.0), idx = cms.int32(-1)), # the default (i.e. no weight)
@@ -25,44 +27,48 @@ tHweights = cms.VPSet(
   cms.PSet(kt = cms.double(1.5),   kv = cms.double(1.0), idx = cms.int32(13)),
   cms.PSet(kt = cms.double(2.0),   kv = cms.double(1.0), idx = cms.int32(14)),
   cms.PSet(kt = cms.double(3.0),   kv = cms.double(1.0), idx = cms.int32(15)),
-  # 17 weights
-  cms.PSet(kt = cms.double(-3.0),  kv = cms.double(1.5), idx = cms.int32(16)),
+  # 17 weights in total - 7 redundant weights -> 10 weights
+  #cms.PSet(kt = cms.double(-3.0),  kv = cms.double(1.5), idx = cms.int32(16)), # has the same kt / kv as idx 1
   cms.PSet(kt = cms.double(-2.0),  kv = cms.double(1.5), idx = cms.int32(17)),
-  cms.PSet(kt = cms.double(-1.5),  kv = cms.double(1.5), idx = cms.int32(18)),
+  #cms.PSet(kt = cms.double(-1.5),  kv = cms.double(1.5), idx = cms.int32(18)), # has the same kt / kv as idx -1
   cms.PSet(kt = cms.double(-1.25), kv = cms.double(1.5), idx = cms.int32(19)),
   cms.PSet(kt = cms.double(-1.0),  kv = cms.double(1.5), idx = cms.int32(20)),
-  cms.PSet(kt = cms.double(-0.75), kv = cms.double(1.5), idx = cms.int32(21)),
+  #cms.PSet(kt = cms.double(-0.75), kv = cms.double(1.5), idx = cms.int32(21)), # has the same kt / kv as idx 4
   cms.PSet(kt = cms.double(-0.5),  kv = cms.double(1.5), idx = cms.int32(22)),
   cms.PSet(kt = cms.double(-0.25), kv = cms.double(1.5), idx = cms.int32(23)),
-  cms.PSet(kt = cms.double(0.0),   kv = cms.double(1.5), idx = cms.int32(24)),
+  #cms.PSet(kt = cms.double(0.0),   kv = cms.double(1.5), idx = cms.int32(24)), # has the same kt / kv as idx 7
   cms.PSet(kt = cms.double(0.25),  kv = cms.double(1.5), idx = cms.int32(25)),
   cms.PSet(kt = cms.double(0.5),   kv = cms.double(1.5), idx = cms.int32(26)),
-  cms.PSet(kt = cms.double(0.75),  kv = cms.double(1.5), idx = cms.int32(27)),
+  #cms.PSet(kt = cms.double(0.75),  kv = cms.double(1.5), idx = cms.int32(27)), # has the same kt / kv as idx 10
   cms.PSet(kt = cms.double(1.0),   kv = cms.double(1.5), idx = cms.int32(28)),
   cms.PSet(kt = cms.double(1.25),  kv = cms.double(1.5), idx = cms.int32(29)),
-  cms.PSet(kt = cms.double(1.5),   kv = cms.double(1.5), idx = cms.int32(30)),
+  #cms.PSet(kt = cms.double(1.5),   kv = cms.double(1.5), idx = cms.int32(30)), # has the same kt / kv as idx 11
   cms.PSet(kt = cms.double(2.0),   kv = cms.double(1.5), idx = cms.int32(31)),
-  cms.PSet(kt = cms.double(3.0),   kv = cms.double(1.5), idx = cms.int32(32)),
-  # 17 weights
+  #cms.PSet(kt = cms.double(3.0),   kv = cms.double(1.5), idx = cms.int32(32)), # has the same kt / kv as idx 14
+  # 17 weights in total - 11 redundant weights -> 6 weights
   cms.PSet(kt = cms.double(-3.0),  kv = cms.double(0.5), idx = cms.int32(33)),
   cms.PSet(kt = cms.double(-2.0),  kv = cms.double(0.5), idx = cms.int32(34)),
-  cms.PSet(kt = cms.double(-1.5),  kv = cms.double(0.5), idx = cms.int32(35)),
+  #cms.PSet(kt = cms.double(-1.5),  kv = cms.double(0.5), idx = cms.int32(35)), # has the same kt / kv as idx 0
   cms.PSet(kt = cms.double(-1.25), kv = cms.double(0.5), idx = cms.int32(36)),
-  cms.PSet(kt = cms.double(-1.0),  kv = cms.double(0.5), idx = cms.int32(37)),
-  cms.PSet(kt = cms.double(-0.75), kv = cms.double(0.5), idx = cms.int32(38)),
-  cms.PSet(kt = cms.double(-0.5),  kv = cms.double(0.5), idx = cms.int32(39)),
-  cms.PSet(kt = cms.double(-0.25), kv = cms.double(0.5), idx = cms.int32(40)),
-  cms.PSet(kt = cms.double(0.0),   kv = cms.double(0.5), idx = cms.int32(41)),
-  cms.PSet(kt = cms.double(0.25),  kv = cms.double(0.5), idx = cms.int32(42)),
-  cms.PSet(kt = cms.double(0.5),   kv = cms.double(0.5), idx = cms.int32(43)),
-  cms.PSet(kt = cms.double(0.75),  kv = cms.double(0.5), idx = cms.int32(44)),
-  cms.PSet(kt = cms.double(1.0),   kv = cms.double(0.5), idx = cms.int32(45)),
+  #cms.PSet(kt = cms.double(-1.0),  kv = cms.double(0.5), idx = cms.int32(37)), # has the same kt / kv as idx 1
+  #cms.PSet(kt = cms.double(-0.75), kv = cms.double(0.5), idx = cms.int32(38)), # has the same kt / kv as idx 2
+  #cms.PSet(kt = cms.double(-0.5),  kv = cms.double(0.5), idx = cms.int32(39)), # has the same kt / kv as idx -1
+  #cms.PSet(kt = cms.double(-0.25), kv = cms.double(0.5), idx = cms.int32(40)), # has the same kt / kv as idx 5
+  #cms.PSet(kt = cms.double(0.0),   kv = cms.double(0.5), idx = cms.int32(41)), # has the same kt / kv as idx 7
+  #cms.PSet(kt = cms.double(0.25),  kv = cms.double(0.5), idx = cms.int32(42)), # has the same kt / kv as idx 9
+  #cms.PSet(kt = cms.double(0.5),   kv = cms.double(0.5), idx = cms.int32(43)), # has the same kt / kv as idx 11
+  #cms.PSet(kt = cms.double(0.75),  kv = cms.double(0.5), idx = cms.int32(44)), # has the same kt / kv as idx 13
+  #cms.PSet(kt = cms.double(1.0),   kv = cms.double(0.5), idx = cms.int32(45)), # has the same kt / kv as idx 14
   cms.PSet(kt = cms.double(1.25),  kv = cms.double(0.5), idx = cms.int32(46)),
-  cms.PSet(kt = cms.double(1.5),   kv = cms.double(0.5), idx = cms.int32(47)),
+  #cms.PSet(kt = cms.double(1.5),   kv = cms.double(0.5), idx = cms.int32(47)), # has the same kt / kv as idx 15
   cms.PSet(kt = cms.double(2.0),   kv = cms.double(0.5), idx = cms.int32(48)),
   cms.PSet(kt = cms.double(3.0),   kv = cms.double(0.5), idx = cms.int32(49)),
+  # verdict: 16 + 10 + 6 = 32 pairs of kt and kv with unique kt / kv ratio
 )
 
 for tHweight in tHweights:
-  tHweight.xs_tHq = cms.double(get_tHq_sf(tHweight.kt.value(), tHweight.kv.value()))
-  tHweight.xs_tHW = cms.double(get_tHW_sf(tHweight.kt.value(), tHweight.kv.value()))
+  kt = tHweight.kt.value()
+  kv = tHweight.kv.value()
+  kt_over_kv = kt / kv
+  tHweight.xs_tHq = cms.double(get_tHq_sf(kt_over_kv, KV_NORM))
+  tHweight.xs_tHW = cms.double(get_tHW_sf(kt_over_kv, KV_NORM))
