a8ac149658341a69bf02512e42ca088f4c4792c1
ktht <karlehataht@gmail.com>
2019-06-08 22:24:42 +0300

    Compute Higgs decay modes if the event contains 2 Higgs bosons
    Reverse the sorting of PDG IDs in case the Higgs has two daughters with different PDG IDs. The only valid case is H->Zg. Previously, the code for H->Zg was 230022 but it's now 220023, i.e. smaller PDG ID comes first. The same principle is used in HH case as well: if the Higgs pair have different daughers (e.g. one Higgs decays to tau pair and the other to W pair), then the decay mode starts with the smallest PDG ID (5000024 in current example).
    See https://github.com/HEP-KBFI/hh-multilepton/issues/1

--- python/postprocessing/modules/genHiggsDecayModeProducer.py
+++ python/postprocessing/modules/genHiggsDecayModeProducer.py
@@ -4,6 +4,30 @@ ROOT.PyConfig.IgnoreCommandLineOptions = True
 from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection
 from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
 
+# Possible values of the decay modes in case the event has one Higgs boson:
+#      3 H -> s sbar
+#      4 H -> c cbar
+#      5 H -> b bbar
+#     13 H -> mu+ mu-
+#     15 H -> tau+ tau-
+#     21 H -> glu glu
+#     22 H -> gamma gamma
+#     23 H -> Z Z
+#     24 H -> W+ W-
+# 220023 H -> Z gamma
+#
+# Possible values of the decay modes in case the event has two Higgs bosons and HH -> 4tau/2V2tau/4V and V = W or Z:
+#       15 H -> tau+ tau- tau+ tau-
+#       23 H -> Z Z Z Z
+#       24 H -> W+ W- W+ W-
+# 15000023 H -> tau+ tau- Z Z
+# 15000024 H -> tau+ tau- W+ W-
+# 23000024 H -> Z Z W+ W-
+#
+# Possible values of the decay modes in case the event has two Higgs bosons and HH -> bbVV and V = W or Z:
+# 5000023 H -> b bbar Z Z
+# 5000024 H -> b bbar W+ W-
+
 class genHiggsDecayModeProducer(Module):
 
   def __init__(self):
@@ -25,24 +49,36 @@ class genHiggsDecayModeProducer(Module):
   def analyze(self, event):
     genParticles = Collection(event, "GenPart")
 
-    genHiggsDecayModeVal = 0
-    nofHiggs = len(list(filter(
+    higgses = sorted(set(map(lambda genPartPair: genPartPair[0], filter(
       lambda genPart:
-        genPart.pdgId == 25 and \
-        (genParticles[genPart.genPartIdxMother].pdgId != 25 if genPart.genPartIdxMother >= 0 else True),
-      genParticles
-    )))
+        genPart[1].pdgId == 25 and \
+        (genParticles[genPart[1].genPartIdxMother].pdgId != 25 if genPart[1].genPartIdxMother >= 0 else True),
+      enumerate(genParticles)
+    ))))
+    nofHiggs = len(higgses)
 
-    if nofHiggs == 1:
-      h0_daus = list(filter(
-        lambda genParticle: genParticle.genPartIdxMother >= 0 and \
-                            genParticles[genParticle.genPartIdxMother].pdgId == 25 and \
-                            genParticle.pdgId != 25,
-        genParticles
-      ))
-      genHiggsDecayModeVal  = abs(h0_daus[0].pdgId if len(h0_daus) >= 1 else 0)
-      genHiggsDecayModeVal += abs(h0_daus[1].pdgId if len(h0_daus) >= 2 and \
-                                                      abs(h0_daus[0].pdgId) != abs(h0_daus[1].pdgId) else 0) * 10000
+    decayModeVals = []
+    if 0 < nofHiggs < 3:
+      for idxHiggs in range(nofHiggs):
+        higgs_daus = [  ]
+        higgs_idx = higgses[idxHiggs]
+        while True:
+          for idx, genPart in enumerate(genParticles):
+            if genPart.genPartIdxMother == higgs_idx:
+              if genPart.pdgId == 25:
+                higgs_idx = idx
+                break
+              else:
+                higgs_daus.append(idx)
+          if len(higgs_daus) == 2:
+            break
+        higgs_daus_pdgId = list(sorted(set(map(lambda idx: abs(genParticles[idx].pdgId), higgs_daus)), reverse = True))
+        decayModeVal = sum(map(lambda pdgId: pdgId[1] * 10**(4 * pdgId[0]), enumerate(higgs_daus_pdgId)))
+        decayModeVals.append(decayModeVal)
+
+    genHiggsDecayModeVal = sum(map(
+      lambda decMode: decMode[1] * 10**(6 * decMode[0]), enumerate(sorted(set(decayModeVals), reverse = True))
+    ))
 
     self.out.fillBranch(self.genHiggsDecayModeName, genHiggsDecayModeVal)
     return True
