ad0e69e26e91afeaae13c41eb2d72b19bf9d6eb5
ktht <karlehataht@gmail.com>
2020-12-24 22:26:55 +0200

    Use regrouped JES
    https://github.com/HEP-KBFI/tth-htt/issues/163

--- python/postprocessing/tthModules.py
+++ python/postprocessing/tthModules.py
@@ -19,17 +19,21 @@ from tthAnalysis.NanoAODTools.postprocessing.modules.rleProducer import rle
 from PhysicsTools.NanoAODTools.postprocessing.modules.common.puWeightProducer import puWeight2016, puWeight2017, puWeight2018
 from PhysicsTools.NanoAODTools.postprocessing.modules.jme.jetmetHelperRun2 import createJMECorrector
 
-jetmetUncertainties2016All = createJMECorrector(dataYear = 2016, jesUncert = "All")
-jetmetUncertainties2017All = createJMECorrector(dataYear = 2017, jesUncert = "All", metBranchName = "MET")
-jetmetUncertainties2018All = createJMECorrector(dataYear = 2018, jesUncert = "All")
+jetmetUncertainties2016Merged = createJMECorrector(dataYear = 2016, jesUncert = "Merged")
+jetmetUncertainties2017Merged = createJMECorrector(dataYear = 2017, jesUncert = "Merged", metBranchName = "MET")
+jetmetUncertainties2018Merged = createJMECorrector(dataYear = 2018, jesUncert = "Merged")
 
 fatjetUncertainties2016Total = createJMECorrector(dataYear = 2016, jesUncert = "Total", jetType = "AK8PFPuppi")
 fatjetUncertainties2017Total = createJMECorrector(dataYear = 2017, jesUncert = "Total", jetType = "AK8PFPuppi")
 fatjetUncertainties2018Total = createJMECorrector(dataYear = 2018, jesUncert = "Total", jetType = "AK8PFPuppi")
 
-jetmetAK4LSLooseUncertainties2016Total = createJMECorrector(dataYear = 2016, jesUncert = "All", jetType = "AK4LSLoosePFchs")
-jetmetAK4LSLooseUncertainties2017Total = createJMECorrector(dataYear = 2017, jesUncert = "All", jetType = "AK4LSLoosePFchs", metBranchName = "MET")
-jetmetAK4LSLooseUncertainties2018Total = createJMECorrector(dataYear = 2018, jesUncert = "All", jetType = "AK4LSLoosePFchs")
+fatjetUncertainties2016Merged = createJMECorrector(dataYear = 2016, jesUncert = "Merged", jetType = "AK8PFPuppi")
+fatjetUncertainties2017Merged = createJMECorrector(dataYear = 2017, jesUncert = "Merged", jetType = "AK8PFPuppi")
+fatjetUncertainties2018Merged = createJMECorrector(dataYear = 2018, jesUncert = "Merged", jetType = "AK8PFPuppi")
+
+jetmetAK4LSLooseUncertainties2016Total = createJMECorrector(dataYear = 2016, jesUncert = "Merged", jetType = "AK4LSLoosePFchs")
+jetmetAK4LSLooseUncertainties2017Total = createJMECorrector(dataYear = 2017, jesUncert = "Merged", jetType = "AK4LSLoosePFchs", metBranchName = "MET")
+jetmetAK4LSLooseUncertainties2018Total = createJMECorrector(dataYear = 2018, jesUncert = "Merged", jetType = "AK4LSLoosePFchs")
 
 fatjetAK8LSLooseUncertainties2016Total = createJMECorrector(dataYear = 2016, jesUncert = "Total", jetType = "AK8LSLoosePFPuppi")
 fatjetAK8LSLooseUncertainties2017Total = createJMECorrector(dataYear = 2017, jesUncert = "Total", jetType = "AK8LSLoosePFPuppi")
